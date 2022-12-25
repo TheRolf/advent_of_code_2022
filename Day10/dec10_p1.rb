42ca6428cf732fde9e4f0a92c5dfc987d03a335b
TheRolf <TheRolf@users.noreply.github.com>
2022-12-25 21:40:37 +0100

    [Day10] Add solution p2

--- Day10/dec10_p1.rb
+++ Day10/dec10_p1.rb
@@ -2,15 +2,27 @@ require 'pp'
 
 
 def signal_strength(i, x)
-    if [20, 60, 100, 140, 180, 220].include? i.to_i
-        pp [i, x, i*x]
+    if [20, 60, 100, 140, 180, 220].include? i
+        #pp [i, x, i*x]
         return i*x
     else
         return 0
     end    
 end
 
-def dec10p01
+def draw(i, x)
+	if i%40 == 0
+		puts
+	end
+	if i%40-1 <= x and x <= i%40+1
+		print "#"
+	else
+		print "."
+	end
+
+end
+
+def dec10
     x = 1
     i = 1
     total_signal_strength = 0
@@ -19,20 +31,23 @@ def dec10p01
         line = line.strip()
         sline = line.split(" ")
         if sline[0] == "noop"
-            pp [i, x, line]
+            #pp [i, x, line]
             total_signal_strength = total_signal_strength + signal_strength(i, x)
+			draw(i, x)
             i = i+1
         elsif sline[0] == "addx"
-            pp [i, x, line]
+            #pp [i, x, line]
             total_signal_strength = total_signal_strength + signal_strength(i, x)
+			draw(i, x)
             i = i+1
-            pp [i, x, line]
+            #pp [i, x, line]
             total_signal_strength = total_signal_strength + signal_strength(i, x)
+			x = x + sline[1].to_i
+			draw(i, x)
             i = i+1
-            x = x + sline[1].to_i
         end
     }
     puts total_signal_strength
 end
 
-dec10p01
+dec10 # RZHFGJCB
